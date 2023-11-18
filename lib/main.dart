ab04ed2eebe91c333c1aa69b15f4d5b9de49a77f
alexaderell <alexander.ellnestam@gmail.com>
2023-11-18 18:30:06 +0100

    added note view as home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:progprojekt/views/note_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.dark
       ),
-      home: Scaffold(),
+      home: const NoteView(),
     );
   }
 }
