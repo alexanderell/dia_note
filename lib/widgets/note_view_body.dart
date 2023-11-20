39cb433a4c4179c8738b341c22db20703521d354
alexaderell <alexander.ellnestam@gmail.com>
2023-11-20 17:52:42 +0100

    Added appbar file

---
+++ lib/widgets/note_view_body.dart
@@ -0,0 +1,9 @@
+class NoteViewBody extends StatelessWidget{
+  const NoteViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold();
+  }
+}
