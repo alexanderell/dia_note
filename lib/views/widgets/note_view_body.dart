da269662e65b03d3e458161c744ffdb1f7e6643c
alexaderell <alexander.ellnestam@gmail.com>
2023-11-21 17:54:50 +0100

    Extracted class note item and created new file

--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NoteViewBody extends StatelessWidget {
   const NoteViewBody({super.key});
@@ -22,28 +23,3 @@ class NoteViewBody extends StatelessWidget {
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('Flutter tips'),
-            subtitle: Text('build your career'),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
