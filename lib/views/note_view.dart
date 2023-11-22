3cf4f3787dd0da117f3d1fb2d37767f54f78456f
alexaderell <alexander.ellnestam@gmail.com>
2023-11-22 18:05:47 +0100

    Extracted note sheet class

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:progprojekt/views/widgets/note_bottom_sheet.dart';
 import 'package:progprojekt/views/widgets/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
@@ -8,10 +9,17 @@ class NoteView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: (context)
+          {
+            return const NoteBottomSheet();
+          }
+          );
+        },
         child: const Icon(Icons.add),
       ),
       body: const NoteViewBody(),
     );
   }
 }
+
