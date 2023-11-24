e9f1f205c3f54aca4e709f587f51785eb7ce3150
alexaderell <alexander.ellnestam@gmail.com>
2023-11-24 21:44:47 +0100

    Initialized hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:progprojekt/constants.dart';
 import 'package:progprojekt/views/note_view.dart';
 
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
