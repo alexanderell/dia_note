e0a5ae5dd16e542c0d03bb07114ad413ae7f9332
alexaderell <alexander.ellnestam@gmail.com>
2023-12-02 16:26:48 +0100

    Add note loading

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,28 +2,41 @@ import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
-class CustomButton extends StatelessWidget{
-  const CustomButton({super.key, this.onTap});
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   final void Function()? onTap;
+  final bool isLoading;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 55,
         decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: const Center(
-          child: Text('Add', style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),),
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(
+              8,
+            )),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
         ),
       ),
     );
