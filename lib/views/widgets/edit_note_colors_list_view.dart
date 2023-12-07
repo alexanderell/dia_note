b38eaa99a9d07e98f090a0c1feaa7bdf0a330e03
alexaderell <alexander.ellnestam@gmail.com>
2023-12-07 17:23:55 +0100

    Added colors to edit note

--- lib/views/widgets/edit_note_colors_list_view.dart
+++ lib/views/widgets/edit_note_colors_list_view.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 import '../../models/note_model.dart';
+import 'colors_list_view.dart';
 
 class EditNoteColorsList extends StatefulWidget {
   const EditNoteColorsList({super.key, required this.note});
 
   final NoteModel note;
+
   @override
   State<EditNoteColorsList> createState() => _EditNoteColorsListState();
 }
@@ -21,7 +23,29 @@ class _EditNoteColorsListState extends State<EditNoteColorsList> {
   }
 
   @override
-  Widget build(BuildContext context){
-    return SizedBox();
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+        itemCount: kColors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                widget.note.color = kColors[index].value;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: kColors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
-  }
+}
